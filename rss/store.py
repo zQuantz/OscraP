742583cb604e4f4fe92526f4b737e28a3838f001
zQuantz <zach.barillaro@gmail.com>
2020-03-20 16:23:14 -0400

    added rss script that stores the collected data

--- rss/store.py
+++ rss/store.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from google.cloud import storage
 from const import DIR
 import pandas as pd
 import sys, os
@@ -11,11 +12,25 @@ def move_files():
 	folder_name = f'{DIR}/news_data_backup/{folder_name}'
 	os.mkdir(folder_name)
 
-	for file in os.listdir(f"{DIR}/news_data"):
+	files = os.listdir(f"{DIR}/news_data")
+	files.remove('.gitignore')
+	
+	for file in files:
 		os.rename(f'{DIR}/news_data/{file}', f"{folder_name}/{file}")
 
 	shutil.make_archive(folder_name, "zip", f"{folder_name}/..")
+	return folder_name + '.zip'
+
+def send_to_bucket(zip_folder):
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket("oscrap_storage")
+
+    destination_name = zip_folder.split('/')[-1]
+    blob = bucket.blob(destination_name)
+    blob.upload_from_filename(zip_folder)
 
 if __name__ == '__main__':
 
-	move_files()
+	zip_folder = move_files()
+	send_to_bucket(zip_folder)
