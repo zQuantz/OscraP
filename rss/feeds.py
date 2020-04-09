ffc380e0fd18adaa968fb712bc39f7361d4ec722
zQuantz <zach.barillaro@gmail.com>
2020-04-09 13:26:56 -0400

    new rss feed collector. no parallel. save objects

--- rss/feeds.py
+++ rss/feeds.py
@@ -14,80 +14,80 @@ import json
 import uuid
 
 class Feeds(Thread):
-    
-    def __init__(self, sources, feeds, sleep, logger):
+	
+	def __init__(self, sources, feeds, sleep, logger):
 
-        Thread.__init__(self)
+		Thread.__init__(self)
 
-        self.sleep = sleep
-        self.logger = logger
-        
-        self.coords = deque([
-            (source.strip(), feed.strip())
-            for source, feed in zip(sources, feeds)
-        ])
+		self.sleep = sleep
+		self.logger = logger
+		
+		self.coords = deque([
+			(source.strip(), feed.strip())
+			for source, feed in zip(sources, feeds)
+		])
 
-        self.entries = []
-        self.last_45 = {
-            feed : []
-            for _, feed in self.coords
-        }
+		self.entries = []
+		self.last_45 = {
+			feed : []
+			for _, feed in self.coords
+		}
 
-    def run(self):
+	def run(self):
 
-        job_defaults = {
-            'coalesce': True,
-            'max_instances': 2
-        }
-        self.blocker = BlockingScheduler(job_defaults = job_defaults)
-        self.blocker.add_job(self.parse_feed, 'cron', second=f'*/{self.sleep}', id='parse_feed')
-        
-        self.blocker.start()
+		job_defaults = {
+			'coalesce': True,
+			'max_instances': 2
+		}
+		self.blocker = BlockingScheduler(job_defaults = job_defaults)
+		self.blocker.add_job(self.parse_feed, 'cron', second=f'*/{self.sleep}', id='parse_feed')
+		
+		self.blocker.start()
 
-    def on_close(self):
+	def on_close(self):
 
-        self.blocker.shutdown()
-        self.join()
-        
-    def parse_feed(self):
+		self.blocker.shutdown()
+		self.join()
+		
+	def parse_feed(self):
 
-        self.coords.rotate()
-        self.source, self.feed = self.coords[0]
-        
-        print(f"Parsing,{self.source},{self.feed},")
-        self.logger.info(f"Parsing,{self.source},{self.feed},")
-        response = feedparser.parse(self.feed)
-        
-        status = response.get('status', None)
-        if not status:
-            self.logger.warning(f"Status,{self.source},{self.feed},None")
-            return
+		self.coords.rotate()
+		self.source, self.feed = self.coords[0]
+		
+		print(f"Parsing,{self.source},{self.feed},")
+		self.logger.info(f"Parsing,{self.source},{self.feed},")
+		response = feedparser.parse(self.feed)
+		
+		status = response.get('status', None)
+		if not status:
+			self.logger.warning(f"Status,{self.source},{self.feed},None")
+			return
 
-        if status != 200:
-            self.logger.warning(f"Status,{self.source},{self.feed},{status}")
-            return
-        self.logger.info(f"Status,{self.source},{self.feed},200")
-        
-        entries = response.get('entries', None)
-        if not entries:
-            self.logger.warning(f"Entries,{self.source},{self.feed},None")
-            return
-        self.logger.info(f"Status,{self.source},{self.feed},{len(entries)}")
+		if status != 200:
+			self.logger.warning(f"Status,{self.source},{self.feed},{status}")
+			return
+		self.logger.info(f"Status,{self.source},{self.feed},200")
+		
+		entries = response.get('entries', None)
+		if not entries:
+			self.logger.warning(f"Entries,{self.source},{self.feed},None")
+			return
+		self.logger.info(f"Status,{self.source},{self.feed},{len(entries)}")
 
-        for entry in entries:
-            
-            entry_str = json.dumps(entry).encode()
-            entry_hash = md5(entry_str).hexdigest()
-            
-            if entry_hash in self.last_45[self.feed]:
-                break
+		for entry in entries:
+			
+			entry_str = json.dumps(entry).encode()
+			entry_hash = md5(entry_str).hexdigest()
+			
+			if entry_hash in self.last_45[self.feed]:
+				break
 
-            self.last_45[self.feed].append(entry_hash)
-            self.last_45[self.feed] = self.last_45[self.feed][-45:]
+			self.last_45[self.feed].append(entry_hash)
+			self.last_45[self.feed] = self.last_45[self.feed][-45:]
 
-            self.entries.append(entry)
+			self.entries.append(entry)
 
-        if len(self.entries) > 0:
-            with open(f"{DIR}/news_data/{str(uuid.uuid4())}.pkl", "wb") as file:
-                joblib.dump(self.entries, file)
-            self.entries = []
+		if len(self.entries) > 0:
+			with open(f"{DIR}/news_data/{str(uuid.uuid4())}.pkl", "wb") as file:
+				joblib.dump(self.entries, file)
+			self.entries = []
--- rss/job.py
+++ rss/job.py
@@ -31,4 +31,3 @@ if __name__ == '__main__':
 			logger = logger
 		)
 		feed_threads[group].start()
-
